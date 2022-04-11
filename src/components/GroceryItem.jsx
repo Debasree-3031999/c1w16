81a034fd16042e2d988b47ca1a7b8b21563b328c
Debasree-3031999 <debasreeghosh1999@gmail.com>
2022-04-12 00:52:37 +0530

    abcd

--- src/components/GroceryItem.jsx
+++ src/components/GroceryItem.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 import  './Grocery.css'
 const GroceryItem = (props) => {
   console.log('props:',props)
-  const{id,title,imgURL,discount,sellingPrice,mrp}=props
+  const{title,imgURL,discount,sellingPrice,mrp}=props
   return (
     <div id='main'>
-      <img src='{imgURL}'/>
-      <P>{title}</P>
+      <img src={imgURL}/>
+      <p>{title}</p>
       <p>{discount}</p>
       <p>{sellingPrice}</p>
       <p>{mrp}</p>
